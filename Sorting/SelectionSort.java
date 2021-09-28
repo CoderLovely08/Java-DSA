40ea5bd2302a66ad13d99e801498cca46fa9f95f
Lovely Sharma <lovelysingeras297@gmail.com>
2021-09-28 20:21:15 +0530

    Added Selection sort algorithm

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,39 @@
+package com.datastructures.Sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+  static int[] sort(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i + 1; j < arr.length; j++) {
+        System.out.println("comparing " + arr[i] + " with " + arr[j]);
+        if (arr[i] > arr[j]) {
+          int temp = arr[j];
+          arr[j] = arr[i];
+          arr[i] = temp;
+          System.out.println("swapped " + arr[i] + " with " + arr[j]);
+        }
+        System.out.println("\n" + Arrays.toString(arr));
+      }
+      System.out.println("\nArray after pass " + (i + 1) + " " + Arrays.toString(arr));
+    }
+    return arr;
+  }
+
+  static int findMin(int[] arr) {
+    int min = Integer.MAX_VALUE, index = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (min > arr[i]) {
+        min = arr[i];
+        index = i;
+      }
+    }
+    return index;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = {4, 2, 6, 89, 1, 3};
+    System.out.println(Arrays.toString(arr));
+    System.out.print(Arrays.toString(sort(arr)));
+  }
+}
