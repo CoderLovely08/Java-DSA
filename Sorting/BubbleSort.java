6ca9e7927171d28e778a86928858ec0685c339da
Lovely Sharma <lovelysingeras297@gmail.com>
2021-09-28 12:47:16 +0530

    Added Bubble sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -6,7 +6,7 @@ public class BubbleSort {
 
   static int[] bubbleSort(int[] arr) {
     for (int i = 0; i < arr.length; i++) {
-      for (int j = i + 1; j < arr.length; j++) {
+      for (int j = 1 + i; j < arr.length; j++) {
         if (arr[j] < arr[j - 1]) {
           int temp = arr[i];
           arr[i] = arr[j];
@@ -17,8 +17,22 @@ public class BubbleSort {
     return arr;
   }
 
+  static int[] bubbleSort2(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 1; j < arr.length - i; j++) {
+        if (arr[j] < arr[j - 1]) {
+          int temp = arr[j];
+          arr[j] = arr[j - 1];
+          arr[j - 1] = temp;
+        }
+      }
+    }
+    return arr;
+  }
+
   public static void main(String[] args) {
     int[] arr = {5, 4, 3, 2, 1};
-    System.out.print(Arrays.toString(bubbleSort(arr)));
+    System.out.println(Arrays.toString(bubbleSort(arr)));
+    System.out.println(Arrays.toString(bubbleSort2(arr)));
   }
 }
