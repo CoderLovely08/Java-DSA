afbbff3ae2f3c3626c04648cd5333e483ca71137
Lovely Sharma <lovelysingeras297@gmail.com>
2021-09-28 20:20:14 +0530

    updated bubble sort and added debugging

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -5,22 +5,10 @@ import java.util.Arrays;
 public class BubbleSort {
 
   static int[] bubbleSort(int[] arr) {
-    for (int i = 0; i < arr.length; i++) {
-      for (int j = 1 + i; j < arr.length; j++) {
-        if (arr[j] < arr[j - 1]) {
-          int temp = arr[i];
-          arr[i] = arr[j];
-          arr[j] = temp;
-        }
-      }
-    }
-    return arr;
-  }
-
-  static int[] bubbleSort2(int[] arr) {
     boolean isSwapped = false;
     for (int i = 0; i < arr.length; i++) {
       for (int j = 1; j < arr.length - i; j++) {
+        System.out.println("comparing " + arr[i] + " with " + arr[j]);
         if (arr[j] < arr[j - 1]) {
           int temp = arr[j];
           arr[j] = arr[j - 1];
@@ -31,6 +19,8 @@ public class BubbleSort {
       if (!isSwapped) {
         break;
       }
+      // debugging
+      System.out.println("\nArray after pass " + (i + 1) + " " + Arrays.toString(arr));
     }
     return arr;
   }
@@ -39,6 +29,5 @@ public class BubbleSort {
     int[] arr = {5, 4, 3, 2, 1};
     int[] arr2 = {1, 2, 3, 4, 5};
     System.out.println(Arrays.toString(bubbleSort(arr)));
-    System.out.println(Arrays.toString(bubbleSort2(arr2    )));
   }
 }
